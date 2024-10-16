b618bd422af537204016c50142940130aa495a3f
João Tasca <142262288+bolamurcha08@users.noreply.github.com>
2024-10-16 11:36:51 -0300

    d

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta,resposta){
+function criacartao(categoria, pergunta,resposta){
 let container = document.getElementByld('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao'
